bac38d4ec38b8df8134702571b876921a1f9b509
aliouswang <wsw622@126.com>
2019-02-21 13:49:13 +0800

    binary search first >=  target

--- lib_algorithm/src/main/java/com/example/lib_algorithm/step1/BinarySearch.java
+++ lib_algorithm/src/main/java/com/example/lib_algorithm/step1/BinarySearch.java
@@ -80,4 +80,22 @@ public class BinarySearch {
         return -1;
     }
 
+    public static int binarySearchFirstEqualOrMore(int[] array, int target) {
+        int low = 0;
+        int high = array.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (array[mid] >= target) {
+                if (mid == 0 || array[mid - 1] < target) {
+                    return mid;
+                }else {
+                    high = mid - 1;
+                }
+            }else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
 }
